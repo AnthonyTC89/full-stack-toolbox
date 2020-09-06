4f550745970f60a252b26b3b309e99add927db31
AnthonyTC89 <ptonyptc@gmail.com>
2020-09-06 15:49:25 -0500

    express initialized; data Routes created

---
+++ src/express.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const dataRoutes = require('./routes/data');
+
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3001);
+
+// MiddleWares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.use(dataRoutes);
+
+// Static Files
+const index = path.join(__dirname, '../build');
+app.use(express.static(index));
+
+// Stating Server
+app.listen(app.get('port'), () => {
+  console.log(`Server on Port ${app.get('port')}`);
+});
---
+++ src/routes/data.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const data = ['hola', 'mundo'];
+
+const router = express.Router();
+router.get('/api/data', async (req, res) => {
+  try {
+    res.json(data);
+  } catch (err) {
+    res.sendStatus(404);
+  }
+});
+
+router.post('/api/data', async (req, res) => {
+  try {
+    const { text } = req.body;
+    data.push(text);
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(404);
+  }
+});
+
+router.delete('/api/data/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    data.splice(id, 1);
+    res.sendStatus(200);
+  } catch (err) {
+    res.sendStatus(400);
+  }
+});
+
+module.exports = router;
