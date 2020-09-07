4976e4a6dfbcc913e04acf599304672b5d6f118c
AnthonyTC89 <ptonyptc@gmail.com>
2020-09-07 10:59:22 -0500

    testing apis updated with supertest

--- test/data.js
+++ test/data.js
@@ -1,9 +1,11 @@
-const request = require('supertest');
 const chai = require('chai');
+const request = require('supertest');
+const chaiHttp = require('chai-http');
 
 const app = require('../src/server');
 
 const { expect } = chai;
+chai.use(chaiHttp);
 
 describe('GET data ', () => {
   it('request should be a success array', (done) => {
@@ -17,23 +19,23 @@ describe('GET data ', () => {
   });
 });
 
-// describe('POST data ', () => {
-//   it('request should be success created', (done) => {
-//     chai.request(url)
-//       .post('/api/data')
-//       .send({ text: 'test' })
-//       .end((err, res) => {
-//         expect(res).to.have.status(201);
-//         done();
-//       });
-//   });
-//   it('request should be error without text in body', (done) => {
-//     chai.request(url)
-//       .post('/api/data')
-//       .send({ })
-//       .end((err, res) => {
-//         expect(res).to.have.status(404);
-//         done();
-//       });
-//   });
-// });
+describe('POST data ', () => {
+  it('request should be success created', (done) => {
+    request(app)
+      .post('/api/data')
+      .send({ text: 'test' })
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        done();
+      });
+  });
+  it('request should be error without text in body', (done) => {
+    request(app)
+      .post('/api/data')
+      .send({ })
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+  });
+});
